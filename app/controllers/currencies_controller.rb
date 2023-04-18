374edee75f43050236741f5aa2269eab8ed06802
Nayla Castillo <naylac14@gmail.com>
2023-04-18 21:00:46 +0000

    created new route /forex/:from_currency/:to_currency

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -24,5 +24,26 @@ class CurrenciesController < ApplicationController
     render({ :template => "currency_templates/step_two.html.erb" })
   end
 
+  def convert_currency
+
+    @from_currency = params.fetch("from_currency")
+    @to_currency = params.fetch("to_currency")
+
+
+    url = "https://api.exchangerate.host/convert?from=#{@from_currency}&to=#{@to_currency}"
+
+   
+
+    @conversion_info_raw = open(url).read
+    @conversion_info_parsed = JSON.parse(@conversion_info_raw)
+   
+    #@exchange_hash = fetch("info")
+    @exchange_rate = @conversion_info_parsed.dig( "info", "rate")
+
+    
+
+    
+    render({ :template => "currency_templates/step_three.html.erb" })
+  end
 
 end
